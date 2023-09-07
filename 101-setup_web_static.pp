dc921ee26dbadce579dfa1f90da63965a4ed0261
Haile-Baranto <hailebaranto@gmail.com>
2023-09-08 00:59:43 +0300

    Fix 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,21 +1,51 @@
-# Puppet manifest that sets up web servers for the deployment of web_static
+#!/usr/bin/puppet apply
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
 
-# Install Nginx if it not already installed
+exec { 'remove-current':
+  command => 'rm -rf /data/web_static/current',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
 package { 'nginx':
-  ensure => present,
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
+
+file { ['/var/www', '/var/www/html/index.html', '/var/www/error/404.html']:
+  ensure  => present,
+  mode    => '0755',
+  recurse => true,
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello, World!',
+}
+
+file { '/var/www/error/404.html':
+  content => "Ceci n'est pas une page",
+}
+
 file { ['/data', '/data/web_static', '/data/web_static/releases', '/data/web_static/shared', '/data/web_static/releases/test']:
-  ensure => directory,
+  ensure  => directory,
+  require => Package['nginx'],
 }
+
 file { '/data/web_static/releases/test/index.html':
-  ensure  => file,
-  content => '<html>
-  <head>
-  </head>
-  <body>
-    <h1>Test</h1>
-  </body>
-</html>',
+  content =>
+"<!DOCTYPE html>
+<html lang='en-US'>
+	<head>
+		<title>Home - AirBnB Clone</title>
+	</head>
+	<body>
+		<h1>Welcome to AirBnB!</h1>
+	<body>
+</html>
+",
 }
 
 file { '/data/web_static/current':
@@ -38,7 +68,6 @@ file_line { 'nginx_config':
   require => Package['nginx'],
 }
 
-# Restart Nginx service
 service { 'nginx':
   ensure    => running,
   enable    => true,
