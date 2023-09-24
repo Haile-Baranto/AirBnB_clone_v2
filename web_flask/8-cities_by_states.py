58fcca68e1209d3092d85300245b8572595f7c14
Haile-Baranto <hailebaranto@gmail.com>
2023-09-24 13:19:39 +0300

    Add 8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Flask web application for displaying a list of cities by states
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(exception=None):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a list of all State objects and their associated cities"""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=sorted_states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
